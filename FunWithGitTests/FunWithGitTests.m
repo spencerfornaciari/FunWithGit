8fa45d8f41b6acaad0adb952e7957680ac29e8a0
Spencer Fornaciari <mail@spencerfornaciari.com>
2013-11-07 19:49:48 -0800

    Initial Commit

---
+++ FunWithGitTests/FunWithGitTests.m
@@ -0,0 +1,34 @@
+//
+//  FunWithGitTests.m
+//  FunWithGitTests
+//
+//  Created by Spencer Fornaciari on 11/7/13.
+//  Copyright (c) 2013 Spencer Fornaciari. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FunWithGitTests : XCTestCase
+
+@end
+
+@implementation FunWithGitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
